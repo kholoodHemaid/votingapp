ed8b2dd48345c6fa91ecface84880e6d1def1a99
kholoodHemaid <khemaid1@smail.ucas.edu.ps>
2023-07-08 18:53:18 +0300

    Update Verified_Screen.dart

--- lib/Screens/btnNavgScreen/Verified_Screen.dart
+++ lib/Screens/btnNavgScreen/Verified_Screen.dart
@@ -9,6 +9,69 @@ class verified_screen extends StatefulWidget {
 class _verified_screenState extends State<verified_screen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: constraints.maxHeight * 0.04,
+                    left: constraints.maxWidth * 0.04,
+                  ),
+                  child: Text(
+                    'Verify Your Account',
+                    style: TextStyle(
+                      fontSize: constraints.maxHeight * 0.035,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        // إجراء يتم تنفيذه عند النقر على الأيقونة
+                        // يمكنك تنفيذ رفع الصورة هنا
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: constraints.maxWidth * 0.25,
+                            height: constraints.maxWidth * 0.25,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.black,
+                                width: constraints.maxWidth * 0.01,
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.add_a_photo,
+                              size: constraints.maxWidth * 0.1,
+                            ),
+                          ),
+                          SizedBox(height: constraints.maxHeight * 0.02),
+                          Text(
+                            'Upload your image',
+                            style: TextStyle(
+                              fontSize: constraints.maxHeight * 0.025,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    
+    );
   }
 }
